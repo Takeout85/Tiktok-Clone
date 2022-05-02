cf3c6da09ea9bdfdfa3afc7025a0e41b56738f37
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-02 17:59:37 -0500

    fix: Rename Icons file

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,9 +1,10 @@
 import { useRef, useState } from 'react'
+import VideoDescription from '../VideoDescription'
 import clsx from 'clsx'
 import styles from './styles.module.css'
 import VideoPlayerActions from './VideoPlayerActions'
 
-export default function VideoPlayer ({ src }) {
+export default function VideoPlayer ({ src, albumCover, author, description, songTitle }) {
   const [playing, setPlaying] = useState(false)
   const video = useRef()
 
@@ -28,6 +29,12 @@ export default function VideoPlayer ({ src }) {
       />
       <i className={playerClassName} onClick={handlePlay} />
       <VideoPlayerActions />
+      <VideoDescription
+        albumCover={albumCover}
+        author={author}
+        description={description}
+        songTitle={songTitle}
+      />
     </div>
 
   )
