c91895407dacf45aae9dde842dc7934ef30129c9
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-02 17:53:40 -0500

    feat(Root): Create Song Ticker component to show song title

---
+++ src/components/SongTicker/index.jsx
@@ -0,0 +1,15 @@
+
+import styles from './styles.module.css'
+import MusicIcon from '../Icons/MusicIcon'
+import Marquee from 'react-fast-marquee'
+
+export default function SongTicker ({ songTitle }) {
+  return (
+    <div className={styles.song}>
+      <MusicIcon />
+      <Marquee gradient={false} speed={70}>
+        {songTitle}
+      </Marquee>
+    </div>
+  )
+}
